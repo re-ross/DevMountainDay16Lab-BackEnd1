732a6057cfb88b99a0ef5a4812a9a67f424426a8
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-24 13:58:26 -0600

    finished building back end lab practice

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
-const cors = requires("cors");
+const cors = require("cors");
+const { get } = require("http");
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/api/users", (req, res) => {
+  let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+  res.status(200).send(friends);
+});
+
+app.get("/weather/:temperature", (req, res) => {
+  const { temperature } = req.params;
+  const phrase = `<h3> It was ${temperature} today</h3>`;
+  res.status(200).send(phrase);
+});
+
 app.listen(4000, () => console.log("Server running on port 4000"));
